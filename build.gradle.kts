355c5ac4905d88bc22de72f6ebebb5a8ff3f5150
gwieshammer <gwieshammer@gmail.com>
2025-05-28 11:35:24 +0200

    v0.8.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "com.phactum.lpm"
-version = "0.8.4-SNAPSHOT"
+version = "0.8.4"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
--- src/main/kotlin/com/phactum/lpm/annotation/StaticSignal.kt
+++ src/main/kotlin/com/phactum/lpm/annotation/StaticSignal.kt
@@ -1,7 +1,5 @@
 package com.phactum.lpm.annotation
 
-import com.phactum.lpm.model.ProcessState
-
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FUNCTION)
 annotation class StaticSignal
